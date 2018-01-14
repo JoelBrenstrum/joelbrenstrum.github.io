0da169cd8525fda968ec17c9af287b396636b9bf
Joel Brenstrum <joel.brenstrum@gmail.com>
2018-01-15 09:26:27 +1300

    revamp

--- scripts/viewmodels/ViewEnum.ts
+++
@@ -1,9 +0,0 @@
-export module ViewEnum {
-    export const index = "index";
-    export const projects = "projects";
-    export const contact = "contact";
-    export const cv = "cv";
-
-};
-
-export type ViewEnum = 'index' | 'projects' | 'contact' | 'cv' | 'sharedJson';
