c7c833cb62deb4bc738173c909c9d2fee6f3dce6
Joel Brenstrum <joelbrenstrum@Joels-MacBook-Pro.local>
2018-11-17 23:07:42 +1300

    Updates

---
+++ precache-manifest.417477abb5371b79add8eefafd24f638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "1623df60d394d912d9b8",
+    "url": "/react/static/js/main.1623df60.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/react/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "1623df60d394d912d9b8",
+    "url": "/react/static/css/main.768781ac.chunk.css"
+  },
+  {
+    "revision": "0c0f7a541e85f102be0a38362f480fc0",
+    "url": "/react/index.html"
+  }
+];
